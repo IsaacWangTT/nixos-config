6c884aab3f260813fbe406d8a4037412c56492ea
Isaac Wang <isaacwangtt@gmail.com>
2023-12-30 18:24:25 +0800

    Add some effect preset

--- home/programs/audio/easyeffects.nix
+++ home/programs/audio/easyeffects.nix
@@ -1,5 +1,19 @@
+{ pkgs, ... }:
+let
+  EasyPulse = pkgs.fetchFromGitHub {
+    owner = "p-chan5";
+    repo = "EasyPulse";
+    rev = "1.2.0";
+    hash = "sha256-UaEMlsV1+yNvefA6O8R7hgK2ZJ08zoqj29nzUAOFjmY=";
+  };
+in
 {
+  home.file.".config/easyeffects/output" = {
+    source = "${EasyPulse}/output";
+    recursive = true;
+  };
   services.easyeffects = {
     enable = true;
+    preset = "classical";
   };
 }
