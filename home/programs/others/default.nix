21114917c8bfa8d14ccd16327f1795c630669128
Isaac Wang <isaacwangtt@gmail.com>
2024-03-01 19:24:54 +0800

    remove telegram, add 64gram

--- home/programs/others/default.nix
+++ home/programs/others/default.nix
@@ -41,7 +41,22 @@
       });
     })
 
-    telegram-desktop
+    (telegram-desktop.overrideAttrs
+      (_: rec {
+
+        pname = "64Gram";
+        version = "1.1.14";
+
+        src = fetchFromGitHub {
+          owner = "TDesktop-x64";
+          repo = "tdesktop";
+          rev = "v${version}";
+
+          fetchSubmodules = true;
+          hash = "sha256-+Cx4qh/zHyBYRBxeZLZATU2U/r8xF24R8AXnfFwl+Oo=";
+        };
+      })
+    )
     qq
 
     # Development
